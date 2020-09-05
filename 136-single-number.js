10ebfe61782e738c1efbf65b6b65dc37f0e23737
Fr3ud <oo.pavelko@gmail.com>
2020-09-05 23:15:28 +0300

    Add 136. Single Number

---
+++ 136-single-number.js
@@ -0,0 +1,29 @@
+/**
+ * Given a non-empty array of integers, every element appears twice except for one. 
+ * Find that single one.
+ */
+
+{
+    const input = [4, 1, 2, 1, 2];
+
+    const singleNumber = (nums) => {
+        let uniq = new Set();
+        let uniqSum = 0;
+        let numSum = 0;
+
+        for (let i = 0; i < nums.length; i++) {
+            const current = nums[i];
+
+            if (!uniq.has(current)) {
+                uniq.add(current);
+                uniqSum += current;
+            }
+
+            numSum += current;
+        }
+
+        return uniqSum * 2 - numSum;
+    };
+
+    console.log(singleNumber(input));
+}
